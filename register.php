35541212f0acbf15255aebc5c27acd6b0ba37dcf
James Randolph <james.randolph2016@gmail.com>
2020-06-26 13:54:45 -0400

    username generation

--- register.php
+++ register.php
@@ -87,6 +87,22 @@ if(isset($_POST['register_button'])){
             array_push($error_array, "Your password must be between 5 and 30 characters<br>");
         }
 
+        if(empty($error_array)) {
+            $password = md5($password); //encrypt pw before sending to database
+
+            // Generate username
+            $username = strtolower($fname . "_" . $lname);
+            $check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$username");
+
+            $i = 0;
+            // If username exists, add number to username
+            while(mysqli_num_rows($check_username_query) != 0) {
+                $i++;
+                $username = $username . "_" . $i;
+                $check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$username'");
+            }
+        }
+
     }
 
 ?>
