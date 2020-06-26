5ef3767251d6d00ff410a1bd33d27a255612549f
James Randolph <james.randolph2016@gmail.com>
2020-06-25 21:35:03 -0400

    clean up form data

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ $con = mysqli_connect("localhost", "root", "", "twitter");
 if(mysqli_connect_errno()) {
     echo("Failed to connect: " . mysqli_connect_errno());
 }
-
-$query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'testing name')");
 ?>
 
 
--- register.php
+++ register.php
@@ -1,10 +1,51 @@
+<? 
+$con = mysqli_connect("localhost", "root", "", "twitter");
+
+if(mysqli_connect_errno()) {
+    echo("Failed to connect: " . mysqli_connect_errno());
+}
+
+// Declaring variable to prevent errors
+$fname = "";
+$lname = "";
+$em = "";
+$em2 = "";
+$password = "";
+$password2 = "";
+$date = "";
+$error_array = "";
+
+if(isset($_POST['register_button'])){
+
+    //Registration form
+    $fname = strip_tags($_POST['reg_fname']);
+    $fname = str_replace(' ', '', $fname);
+    $fname = ucfirst(strtolower($fname));
+
+    $lname = strip_tags($_POST['reg_lname']);
+    $lname = str_replace(' ', '', $lname);
+    $lname = ucfirst(strtolower($lname));
+
+    $em = strip_tags($_POST['reg_email']);
+    $em = str_replace(' ', '', $em);
+    $em = ucfirst(strtolower($em));
+
+    $em2 = strip_tags($_POST['reg_email2']);
+    $em2 = str_replace(' ', '', $em2);
+    $em2 = ucfirst(strtolower($em2));
+
+    $password = strip_tags($_POST['reg_password']);
+    $password2 = strip_tags($_POST['reg_password2']);
+}
+?>
+
 <html>
 <head>
 <title>Welcome to Basically Twitter</title>
 </head>
 <body>
     
-    <form action="">
+    <form action="register.php" method="POST">
         <input type="text" name="reg_fname" placeholder="First Name" required>
         <br>
         <input type="text" name="reg_lname" placeholder="Last Name" required>
