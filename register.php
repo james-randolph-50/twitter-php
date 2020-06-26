b9fb0ced8d09ed5878869b7a6fc83652d2dafd07
James Randolph <james.randolph2016@gmail.com>
2020-06-25 21:40:19 -0400

    begin email validation

--- register.php
+++ register.php
@@ -36,6 +36,23 @@ if(isset($_POST['register_button'])){
 
     $password = strip_tags($_POST['reg_password']);
     $password2 = strip_tags($_POST['reg_password2']);
+
+    $date = date("Y-m-d");
+
+    if($em == $em2)  {
+        // Check if email is in valid format
+        if(filter_var($em, FILTER_VALIDATE_EMAIL)) {
+            $em = filter_var($em, FILTER_VALIDATE_EMAIL);
+        }
+        else {
+            echo("Invalid email format");
+        }
+
+
+    }
+    else {
+        echo("Emails don't match.");
+    }
 }
 ?>
 
