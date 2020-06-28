d7c8e88cf49c88efe50e7540e23aba657d9a35bc
James Randolph <james.randolph2016@gmail.com>
2020-06-28 12:08:36 -0400

    ajax call to load posts

--- index.php
+++ index.php
@@ -30,12 +30,57 @@ if(isset($_POST['post'])){
             <hr>
         </form>
 
-        <? 
-        $post = new Post($con, $userLoggedIn);
-        $post->loadPostsFriends();
-        
-        ?>
+        <div class="posts_area"></div>
+        <img id="#loading" src="assets/images/icons/loading.png" alt="loading icon">
+
     </div>
+
+    <script>
+        var userLoggedIn = '<? echo($userLoggedIn); ?>';
+
+        $(document).ready(function() {
+            $('#loading').show();
+
+            // Original ajax request for loading first posts
+            $.ajax({
+                url: "includes/handlers/ajax_load_posts.php",
+                type: "POST",
+                data: "page=1&userLoggedIn=" + userLoggedIn,
+                cache: false,
+
+                success: function(data){
+                    $('#loading').hide();
+                    $('.posts_area').html(data);
+                }
+            });
+
+            $(window).scroll(function() {
+                var height = $('.posts_area').height(); // div containing posts
+                var scroll_top = $(this).scrollTop();
+                var page = $('.posts_area').find('.nextPage').valu();
+                var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+                if ((docuement.body.scrollHeight == document.body.scrollTop + window.innerheight) && noMorePosts == 'false') {
+                    $('#loading').show();
+
+                    var ajaxReq = $.ajax({
+                        url: "includes/handlers/ajax_load_posts.php",
+                        type: "POST",
+                        data: "page=" + page + "userLoggedIn=" + userLoggedIn,
+                        cache: false,
+
+                        success: function(data){
+                            $('#loading').hide();
+                            $('.posts_area').html(data);
+                        }
+                    });
+
+                }
+            });
+
+
+        });
+    </script>
 
 
 
