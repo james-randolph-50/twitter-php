0461724289b7c5bea119ea4686daca9cc14c9775
James Randolph <james.randolph2016@gmail.com>
2020-06-28 10:20:07 -0400

    post class

---
+++ includes/classes/Post.php
@@ -0,0 +1,36 @@
+<?
+class Post {
+    private $user_obj;
+    private $con;
+
+    public function __construct($con, $user){
+        $this->con = $con;
+        $this->user_obj = new User($con, $user);
+    }
+
+    public function submitPost($body, $user_to) {
+        $body = strip_tags($body); // removes html tags
+        $body = mysqli_real_escape_string($this->con, $body);
+        $check_empty = preg_replace('/\s+/', '', $body); // deletes all spaces
+
+        if($check_empty != "") {
+
+            //Current date and time
+            $date_added = date("Y-m-d H:i:s");
+            // Get username
+            $added_by = $this->user_obj->getUsername();
+
+            // If user is on own profile, user_to is 'none'
+            if($user_to == $added_by) {
+                $user_to = "none";
+            }
+
+
+
+        }
+    }
+
+
+}
+
+?>
