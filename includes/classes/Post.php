165d80c866e38da0c5f9fbb74652937e9d7e8fb6
James Randolph <james.randolph2016@gmail.com>
2020-06-28 10:36:33 -0400

    rerplace link breaks with html klinke breaks

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -11,6 +11,10 @@ class Post {
     public function submitPost($body, $user_to) {
         $body = strip_tags($body); // removes html tags
         $body = mysqli_real_escape_string($this->con, $body);
+
+        $body = str_replace('\r\n', '\n', $body);
+        $body = nl2br($body);
+
         $check_empty = preg_replace('/\s+/', '', $body); // deletes all spaces
 
         if($check_empty != "") {
