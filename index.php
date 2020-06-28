00f90a4a2f5fff913b606c6450d8ff38602af9d6
James Randolph <james.randolph2016@gmail.com>
2020-06-28 09:42:53 -0400

    main feed add

--- index.php
+++ index.php
@@ -15,6 +15,14 @@ include("includes/header.php");
         </div>
     </div>
 
+    <div class="main_column column">
+        <form class="post_form" action="index.php" method="POST">
+            <textarea name="post_text" id="post_text" placeholder="Type your post here..."></textarea>
+            <input type="submit" name="post" id="post_button" value="Post">
+            <hr>
+        </form>
+    </div>
+
 
 
     </div> <!-- this is the wrapper tag that's opened in header -->
