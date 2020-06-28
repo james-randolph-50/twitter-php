99df9b69989f8712beb8d9c286f6d5a050757868
James Randolph <james.randolph2016@gmail.com>
2020-06-28 15:09:46 -0400

    like  functionality and styling

--- like.php
+++ like.php
@@ -6,6 +6,11 @@
         <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     </head>
 <body>
+
+<style>
+body {background:#fff;}
+form {position:absolute;top:0;}
+</style>
 <? 
     require 'config/config.php';
     include("includes/classes/User.php");
@@ -28,24 +33,54 @@
     $get_likes = mysqli_query($con, "SELECT likes, added_by FROM posts WHERE id='$post_id'");
     $row = mysqli_fetch_array($get_likes);
     $total_likes = $row['likes'];
-    $user_liked = row['added_by'];
+    $user_liked = $row['added_by'];
 
     $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user_liked'");
     $row = mysqli_fetch_array($user_details_query);
+    $total_user_likes = $row['num_likes'];
 
     // Like button
+    if(isset($_POST['like_button'])) {
+        $total_likes++;
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'");
+        $total_user_likes++;
+        $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
+        $insert_user = mysqli_query($con, "INSERT INTO likes VALUES(NULL, '$userLoggedIn', '$post_id')");
+
+        // Insert notification
+    }
 
     // Unlike button
+
+    if(isset($_POST['unlike_button'])) {
+        $total_likes--;
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE post_id='$post_id'");
+        $total_user_likes--;
+        $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
+        $insert_user = mysqli_query($con, "DELETE  FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id'");
+    }
 
     // Check for previous likes
     $check_query = mysqli_query($con, "SELECT * FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id'");
     $num_rows = mysqli_num_rows($check_query);
 
-    if($num_row > 0) {
-        echo();
+    if($num_rows > 0) {
+        echo('<form action="like.php?post_id=' . $post_id . '" method="POST">
+            <input type="submit" class="comment_like" name="unlike_button" value="Unlike">
+            <div class="like_value">
+                '. $total_likes .' Likes
+            </div>
+            </form>
+        ');
     }
     else {
-        echo();
+        echo('<form action="like.php?post_id=' . $post_id . '" method="POST">
+        <input type="submit" class="comment_like" name="like_button" value="Like">
+        <div class="like_value">
+            '. $total_likes .' Likes
+        </div>
+        </form>
+    ');
     }
 ?>
 
