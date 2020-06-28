1f536d05fcff73eb0e414a3c4f7e4e2260c414d4
James Randolph <james.randolph2016@gmail.com>
2020-06-28 08:24:57 -0400

    display profile pic

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?
 include("includes/header.php");
 ?>
-Hey there php twitter
+    <div class="user_details column">
+        <a href="#"> <img src="<? echo $user['profile_pic'];?>" /></a>
+    
+    </div>
 
 </body>
 </html>
