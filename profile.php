09fa236c97c6883657c05b88f92652334923295e
James Randolph <james.randolph2016@gmail.com>
2020-06-28 16:01:52 -0400

    adding info to profile page

--- profile.php
+++ profile.php
@@ -1,10 +1,27 @@
 <?
 include("includes/header.php");
+
+if(isset($_GET['profile_username'])) {
+    $username = $_GET['profile_username'];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+    $user_array = mysqli_fetch_array($user_details_query);
+
+    $num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
+}
 ?>
 
+    <div class="profile_left">
+        <img src="<? echo($user_array['profile_pic']);?>" alt="profile picture">
+        
+        <div class="profile_info">
+            <p><? echo("Posts: " . $user_array['num_posts']);?></p>
+            <p><? echo("Likes: " . $user_array['num_likes']);?></p>
+            <p><? echo("Friends: " . $num_friends);?></p>
+        </div>
+    </div>
 
     <div class="main_column column">
-       This is a profile page
+       <? echo($username); ?>
     </div>
 
 
