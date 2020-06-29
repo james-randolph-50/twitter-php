40419b27a0f2d694a6434cf0431e827202f9352e
James Randolph <james.randolph2016@gmail.com>
2020-06-29 19:53:00 -0400

    friend requests working

--- profile.php
+++ profile.php
@@ -10,6 +10,22 @@ if(isset($_GET['profile_username'])) {
 
     $num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
 }
+
+    if(isset($_POST['remove_friend'])) {
+        $user = new User($con, $userLoggedIn);
+        $user->removeFriend($username);
+    }
+
+    if(isset($_POST['add_friend'])) {
+        $user = new User($con, $userLoggedIn);
+        $user->sendRequest($username);
+    }
+
+    if(isset($_POST['respond_request'])) {
+        header("Location: requests.php");
+    }
+
+
 ?>
 
 <style>
@@ -28,7 +44,7 @@ if(isset($_GET['profile_username'])) {
             <p><? echo("Friends: " . $num_friends);?></p>
         </div>
 
-        <form action="<?echo($username);?>">
+        <form action="<?echo($username);?>" method="POST">
             <? 
                 $profile_user_obj = new User($con, $username); 
             
