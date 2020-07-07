e8fae2be6182c1966d27aad2016e6689095cda23
James Randolph <james.randolph2016@gmail.com>
2020-07-07 19:17:07 -0400

    delete button shows

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -63,6 +63,11 @@ class Post {
                         $count++;
                     }
 
+                    if($userLoggedIn == $added_by)
+                        $delete_button = "<button class='delete_button btn-danger' id='post$id'>X</button>";
+                    else
+                        $delete_button = ""; 
+
                     $user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
                     $user_row = mysqli_fetch_array($user_details_query);
                     $first_name = $user_row['first_name'];
@@ -160,6 +165,7 @@ class Post {
 
                                 <div class='posted_by' style='color:#ACACAC;'>
                                     <a href='$added_by'>$first_name $last_name </a> $user_to &nbsp;&nbsp;&nbsp;$time_message
+                                    $delete_button
                                 </div>
 
                                 <div id='post_body'>
