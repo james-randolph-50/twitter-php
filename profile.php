6a75f8458ea3aace82dafe64942fed6f13b233f7
James Randolph <james.randolph2016@gmail.com>
2020-07-06 20:51:57 -0400

    ajax call for post form

--- profile.php
+++ profile.php
@@ -106,7 +106,7 @@ if(isset($_GET['profile_username'])) {
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Post</button>
+        <button type="button" name="post_button" id="submit_profile_post" class="btn btn-primary">Post</button>
       </div>
     </div>
   </div>
