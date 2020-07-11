6119659437e2493990a69e6b6f7b029f61646b68
James Randolph <james.randolph2016@gmail.com>
2020-07-11 09:30:14 -0400

    search for like names base don first, lastm or both. shows mutual firneds

--- includes/handlers/ajax_friend_search.php
+++ includes/handlers/ajax_friend_search.php
@@ -7,4 +7,27 @@ $userLoggedIn = $_POST['userLoggedIn'];
 
 $names = explode(" ", $query);
 
+if(strpos($query, "_") !== false) {
+    $usersReturned = mysqli_query($con, "SELECT * FROM users WHERE username LIKE '$query%' AND user_closed='no' LIMIT 8");
+}
+else if (cound($names) == 2) {
+    $usersreturned = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '%$names[0]%' AND last_name LIKE '%$names[1]%') AND user_closed='no' LIMIT 8");
+}
+else {
+    $usersreturned = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '%$names[0]%' AND last_name LIKE '%$names[0]%') AND user_closed='no' LIMIT 8");
+}
+
+if($query != "") {
+    while($row - mysqli_fetch_array($usersReturned)) {
+        $user = new User($con, $userLoggedIn);
+
+        if($row['username'] != $userLoggedIn) {
+            $mutual_friends = $user->getMutualFriends($row['username'] . " friends in common");
+        }
+        else {
+            $mutual_friends = "";
+        }
+    }
+}
+
 ?>
