8ace83367cf04a1f3f3f3f4ea4992ce2f9723ff6
James Randolph <james.randolph2016@gmail.com>
2020-07-27 21:13:39 -0400

    details update without needing to reload

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 include("includes/header.php");
-include("includes/settings_handler.php");
+include("includes/form_handlers/settings_handler.php");
 
 ?>
 
@@ -13,10 +13,24 @@ include("includes/settings_handler.php");
 
     Modify the values and click 'Update Details'
 
+    <?php
+    $user_data_query = mysqli_query($con, "SELECT first_name, last_name, email FROM users WHERE username='$userLoggedIn'");
+    $row = mysqli_fetch_array($user_data_query);
+
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    $email = $row['email'];
+
+    ?>
+
     <form action="settings.php" method="POST">
-        First Name: <input type="text" name="first_name" value="<? echo $user['first_name'] ?>"><br>
-        Last Name: <input type="text" name="last_name" value="<? echo $user['last_name'] ?>"><br>
-        Email: <input type="text" name="email" value="<? echo $user['email'] ?>"><br>
+        First Name: <input type="text" name="first_name" value="<? echo $first_name; ?>"><br>
+        Last Name: <input type="text" name="last_name" value="<? echo $last_name ?>"><br>
+        Email: <input type="text" name="email" value="<? echo $email ?>"><br>
+
+        <? echo $message; ?>
+
+
         <input type="submit" name="update_details" id="save_details" value="Update Details"><br>
 
     </form>
