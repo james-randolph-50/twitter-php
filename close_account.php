5917f29bd006c2a61fbe5cf4b50feef28fa5891d
James Randolph <james.randolph2016@gmail.com>
2020-07-27 21:36:25 -0400

    closr accout

--- close_account.php
+++ close_account.php
@@ -1,6 +1,16 @@
 <?php
 include("includes/header.php");
 
+if(isset($_POST['cancel'])) {
+    header("Location: settings.php");
+}
+
+if(isset($_POST['close_account'])) {
+    $close_query = mysqli_query($con, "UPDATE users SET user_closed='yes' WHERE username='$userLoggedIn'");
+    session_destroy();
+    header("Location: register.php");
+}
+
 ?>
 
 <div class="main_column column">
